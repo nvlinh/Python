9634b0266063446e10ec6cae3931c365b75abcef
nvlinh <nguyenvanlinh.tl4@gmail.com>
2020-04-16 10:19:12 +0700

    fix comment

--- PythonCrashCourse2nd/Chap_4_WorkingWithLists/list.py
+++ PythonCrashCourse2nd/Chap_4_WorkingWithLists/list.py
@@ -17,7 +17,7 @@ magicians = ['alice', 'david', 'carolina']
 for magician in magicians:
  print(f"{magician.title()}, that was a great trick!")
 print(f"I can't wait to see your next trick, {magician.title()}.\n")
-# code don't error but ony run second print on time.
+# code don't error but ony run second print one time.
 
 #2.3 Indenting unnecessarily
 message = "Hi"
