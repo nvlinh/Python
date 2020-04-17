9b68946d638a325ff9cff65c7df592e262fe1d00
nvlinh <nguyenvanlinh.tl4@gmail.com>
2020-04-17 09:52:11 +0700

    update commit d7035db

--- PythonCrashCourse2nd/Chap_4_WorkingWithLists/README.txt
+++ PythonCrashCourse2nd/Chap_4_WorkingWithLists/README.txt
@@ -3,8 +3,8 @@ uses indentation to structure a program, and how to avoid some common
 indentation errors.
 You will learn to make simple numerical lists, as well as a
 few operations you can perform on numerical lists.
-You will learn how to slicea list to work with a subset of items and how to copy lists properly using a
-slice. 
+You will learn how to slice a list to work with a subset of items and how to
+copy lists properly using a slice.
 You will learn about tuples, which provide a degree of protection
-to a set of values that shouldn’t change, and how to style your increasingly
+to a set of values that shouldn't change, and how to style your increasingly
 complex code to make it easy to read
--- PythonCrashCourse2nd/Chap_4_WorkingWithLists/list.py
+++ PythonCrashCourse2nd/Chap_4_WorkingWithLists/list.py
@@ -1,71 +1,72 @@
-#1. Looping through an entire list
+# 1. Looping through an entire list
 foods = ['fish', 'meat', 'egg']
 for food in foods:
-	print(food)
+    print(food)
 
-#2. Avoid identation error
-print('2. Avoid identation error')
-#2.1 Forgetting to ident
+# 2. Avoid indentation error
+print('2. Avoid indentation error')
+# 2.1 Forgetting to ident
 magicians = ['alice', 'david', 'carolina']
 for magician in magicians:
-#print(magician) => IndentationError
-	print(magician)
+    # if print(magician) don't have indent in the left => IndentationError
+    print(magician)
 
-#2.2 Forgetting to indent additional lines
+# 2.2 Forgetting to indent additional lines
 print('Forgetting to indent additional lines\n')
 magicians = ['alice', 'david', 'carolina']
 for magician in magicians:
- print(f"{magician.title()}, that was a great trick!")
+    print(f"{magician.title()}, that was a great trick!")
 print(f"I can't wait to see your next trick, {magician.title()}.\n")
 # code don't error but ony run second print one time.
 
-#2.3 Indenting unnecessarily
+# 2.3 Indenting unnecessarily
 message = "Hi"
- #print(message) => IndentationError: unexpected indent
+# print(message) => IndentationError: unexpected indent
 
 # 2.4 Indenting unnecessarily after the loop
 print('\n 2.4 Indenting unnecessarily after the loop \n')
 magicians = ['alice', 'david', 'carolina']
 for magician in magicians:
- print(f"{magician.title()}, that was a great trick!")
- print(f"I can't wait to see your next trick, {magician.title()}.\n")
+    print(f"{magician.title()}, that was a great trick!")
+    print(f"I can't wait to see your next trick, {magician.title()}.\n")
 
- print("Thank you everyone, that was a great magic show!") # in for loop becase have indent in the left
+    print("Thank you everyone, that was a great magic show!")
+    # in for loop because have indent in the left
 
 # 2.5 Forgetting the colon
-#for magician in magicians
+# for magician in magicians
 
-# 3. Making numberical lists
-print('\n 3. Making numberical lists \n')
+# 3. Making number lists
+print('\n 3. Making number lists \n')
 
 # 3.1 Using the range() function
-for value in range(1,6):
-	print(value) #print 1,2,3,4,5
+for value in range(1, 6):
+    print(value)  # print 1,2,3,4,5
 
-for value in range (6):
-	print(value) #print 0,1,2,3,4,5
+for value in range(6):
+    print(value)  # print 0,1,2,3,4,5
 
 # 3.2 Using range() to making a list of number
 print('\n 3.2 Using range() to making a list of number \n')
-numbers = list(range(1,11))
+numbers = list(range(1, 11))
 print(numbers)
 
-numbers = list(range(1,11,2))
-print(numbers) 
+numbers = list(range(1, 11, 2))
+print(numbers)
 
 # 3.3 Simple statistics with a list of numbers
 print('\n 3.3 Simple statistics with a list of numbers\n')
-digits = [1,2,3,4,5,6,7,8,9,10]
+digits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 print(max(digits))
 print(min(digits))
 print(sum(digits))
 
 # 3.4 List comprehensions
 print('\n3.4 List comprehensions\n')
-squares = [value**2 for value in range(1,11)]
+squares = [value ** 2 for value in range(1, 11)]
 print(squares)
 
-#4 Working with part of a list
+# 4 Working with part of a list
 print('\n4 Working with part of a list:\n')
 # 4.1 Slicing a List
 numbers = ['One', 'Two', 'Three', 'Four', 'Five', 'Six']
@@ -81,12 +82,12 @@ print('4.2 Looping through a slice:\n')
 players = ['linh', 'tam', 'tinh', 'tien']
 print('Here are the first three player on my team')
 for player in players[:3]:
-	print(player)
+    print(player)
 
 # 4.3 Copying a List
 print(' 4.3 Copying a List:\n')
 my_foods = ['rice', 'fish', 'meat', 'egg']
-my_wife_foods = my_foods[:] 
+my_wife_foods = my_foods[:]
 my_foods.append('coffee')
 my_wife_foods.append('pizza')
 print(f'My foods : {my_foods}')
@@ -95,23 +96,18 @@ print(f'My wife foods {my_wife_foods}')
 # 5 Tuples
 print('5. Tuples\n')
 # An immutable list is called tuple
-#5.1 Defining a Tuple
+# 5.1 Defining a Tuple
 print('5.1 Defining a Tuple\n')
-firstTuple = (1,2,3)
+firstTuple = (1, 2, 3)
 print(firstTuple[0])
-#firstTuple[1] =2 #TypeError: 'tuple' object does not support item assignment
-#defining one element
+# firstTuple[1] =2 #TypeError: 'tuple' object does not support item assignment
+# defining one element
 secondTuple = (4,)
 for item in secondTuple:
-	print(item)
+    print(item)
 # Writing over a Tuple
-firstTuple = (4,5,6,7)
+firstTuple = (4, 5, 6, 7)
 
 # 6. Styling your code
 # A bit about PEP8 : indentation, line length, blank line
-# https://www.python.org/dev/peps/pep-0008/ 
-
-
-
-
-
+# https://www.python.org/dev/peps/pep-0008/
