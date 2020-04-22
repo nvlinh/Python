bf873f1c079fb37a0ef6a50c13590aa45e20e163
nvlinh <nguyenvanlinh.tl4@gmail.com>
2020-04-22 21:03:24 +0700

    [project alien invasion] : piloting the ship

--- PythonCrashCourse2nd/Projects/alien_invasion/settings.py
+++ PythonCrashCourse2nd/Projects/alien_invasion/settings.py
@@ -4,5 +4,7 @@ class Settings:
         """Initialize the game's settings."""
         # Screen settings
         self.screen_width = 1100
-        self.screen_height = 600
+        self.screen_height = 300
         self.bg_color = (135,206,235)
+        # Ship setting
+        self.ship_speed = 1.5
