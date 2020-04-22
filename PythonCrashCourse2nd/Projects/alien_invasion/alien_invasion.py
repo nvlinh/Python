4d2761d25bb2380d6513e8920f8c7cf5ffc69f6f
nvlinh <nguyenvanlinh.tl4@gmail.com>
2020-04-22 15:28:19 +0700

    [project] alien invasion: starting game project

---
+++ PythonCrashCourse2nd/Projects/alien_invasion/alien_invasion.py
@@ -0,0 +1,42 @@
+import sys
+import pygame
+from settings import Settings
+from ship import Ship
+
+
+class AlienInvasion:
+    """Overall class to manage game assets and behavior."""
+
+    def __init__(self):
+        """Initialize the game, and create game resources."""
+        pygame.init()
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode((self.settings.screen_width,
+                                               self.settings.screen_height))
+        self.ship = Ship(self)
+        pygame.display.set_caption("Alien Invasion")
+
+    def run_game(self):
+        """Start the main loop for the game."""
+        while True:
+            self._check_even()
+            self._update_screen()
+
+    def _check_even(self):
+        """Respond to keypresses and mouse events."""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+    def _update_screen(self):
+        """Update images on the screen, and flip to the new screen."""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        # Make the most recently drawn screen visible.
+        pygame.display.flip()
+
+
+if __name__ == '__main__':
+    # Make a game instance, and run the game.
+    ai = AlienInvasion()
+    ai.run_game()
---
+++ PythonCrashCourse2nd/Projects/alien_invasion/settings.py
@@ -0,0 +1,8 @@
+class Settings:
+    """A class to store all settings for Alien Invasion."""
+    def __init__(self):
+        """Initialize the game's settings."""
+        # Screen settings
+        self.screen_width = 1100
+        self.screen_height = 600
+        self.bg_color = (135,206,235)
